dcb06b758bbcbc22ac0447dfa3b8e0329e5ca331
JonoMN <81200835+JonoMN@users.noreply.github.com>
2024-04-23 17:54:19 +1000

    Added folder structure and routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Dashboard from "./pages/dashboard";
+import Accounts from "./pages/accounts";
+import Transactions from "./pages/transactions";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/accounts" element={<Accounts />} />
+        <Route path="/transactions" element={<Transactions />} />
+      </Routes>
     </div>
   );
 }
