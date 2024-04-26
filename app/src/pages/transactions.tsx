0d2251c24f7869776f938df750580ebb1eb47877
JonoMN <81200835+JonoMN@users.noreply.github.com>
2024-04-26 19:44:07 +1000

    Added transactions page functionality

--- app/src/pages/transactions.tsx
+++ app/src/pages/transactions.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import { Grid, Typography } from '@mui/material';
+import TransactionBlock from '../components/TransactionCard';
+import { useTransactions } from '../hooks/useTransactions';
 
 const Transactions = () => {
-    return <div>Transactions</div>;
+    const { transactions } = useTransactions();
+
+    return (
+        <Grid container spacing={2} sx={{ p: 5 }}>
+            <Grid item xs={12}>
+                <Typography variant="h3" sx={{ color: "white" }}>
+                    Transactions
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                {transactions.map((transaction) => {
+                    return (
+                        <TransactionBlock createdAt={transaction.attributes.createdAt} category={transaction.relationships.category.data?.id} description={transaction.attributes.description} amount={transaction.attributes.amount.value} />
+                    )
+                })}
+            </Grid>
+        </Grid>
+    );
 };
 export default Transactions;
